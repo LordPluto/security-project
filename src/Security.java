4029dbcebf5aa4476ca60a0ba99f3feb8f8450b1
Shannon Weir <stw3763@rit.edu>
2017-02-25 15:33:47 -0500

    Basics of hashing passwords and verifying them. Plus some unit testing stuff.

---
+++ src/Security.java
@@ -0,0 +1,106 @@
+/**
+ * Class for static methods used in account verification.
+ * Used to hash passwords and check hashed passwords.
+ * 
+ * 2/25/2017
+ * @author Group 5
+ * 		Shannon Weir
+ * 		David Pyenson
+ * 		Elliot Allen
+ */
+
+//imports
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.util.Arrays;
+import java.util.Random;
+
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+
+public class Security {
+	private static final int salt_iterations = 100;
+	private static final int hash_iterations = 100000;
+	private static final int key_length = 256;
+	
+	/**
+	 * Static utility class cannot be instantiated
+	 */
+	private Security() { }
+	
+	/**
+	 * Produces a random salt based on the user-supplied password
+	 * The salt is 16 bytes, or 104 bits long.
+	 * 
+	 * @param seed		Seed for the random number generator.
+	 * @return			16-byte array for the salt value
+	 */
+	private static byte[] getSalt(int seed) {
+		byte[] salt = new byte[16];
+		Random rand = new Random(seed);
+		for(int count = 0;count < salt_iterations;++count) {
+			rand.nextBytes(salt);
+		}
+		return salt;
+	}
+	
+	/**
+	 * Given a password, hash the password and return.
+	 * Hash is based on PBKDF2 and SHA1
+	 * 
+	 * Note - the password is destroyed as a result of this
+	 * function.
+	 * 
+	 * @param password		Password to be hashed
+	 * @return				salted and hashed password
+	 * @throws AssertionError	Thrown error in case of hash fail.
+	 */
+	public static byte[] hash(String password) 
+		throws AssertionError {
+		char[] passChar = password.toCharArray();
+		PBEKeySpec spec = new PBEKeySpec(passChar, getSalt(password.hashCode()), hash_iterations, key_length);
+		password = "";
+		try {
+			SecretKeyFactory skf = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+			return skf.generateSecret(spec).getEncoded();
+		} catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+			throw new AssertionError("Error while hashing password: " + e.getMessage(), e);
+		} finally {
+			spec.clearPassword();
+		}
+	}
+	
+	/**
+	 * Verifies whether a given password will hash to the same submitted hash value
+	 * 
+	 * @param password		Password to be tested
+	 * @param hashValue		Hash value to test against, created using the Security.hash function
+	 * @return				true if password's hash matches given hash value, false otherwise
+	 */
+	public static boolean verifyPassword(String password, byte[] hashValue) {
+		byte[] pwdHash = hash(password);
+		password = "";
+		if(pwdHash.length != hashValue.length) {
+			//The lengths aren't the same, so they can't be equal.
+			return false;
+		}
+		for(int i = 0;i < pwdHash.length;++i) {
+			if(pwdHash[i] != hashValue[i]) {
+				//Password byte did not match hash byte.
+				return false;
+			}
+		}
+		
+		//At this point, the password hash matches the hash byte
+		return true;
+	}
+	
+	/**
+	 * Returns the length of the key
+	 * 
+	 * @return	Length of key in bytes
+	 */
+	public static int getKeyLength() {
+		return key_length/8;
+	}
+}
---
+++ src/test.java
@@ -0,0 +1,103 @@
+/**
+ * Use this class for unit testing.
+ * 
+ * 2/25/2017
+ * @author Group 5
+ * 		Shannon Weir
+ * 		David Pyenson
+ * 		Elliot Allen
+ */
+
+//imports
+import java.io.*;
+import java.nio.charset.*;
+import java.nio.file.*;
+import java.util.Base64;
+
+public class test {
+	public static void main(String[] args) {
+		testPasswordHashing("abcdefghijkl");
+		
+		testPasswordSaving("testAccount", "abcdefghijkl", "testFile.txt", false);
+		testPasswordSaving("shannon.weir", "helloWorld", "testFile.txt", true);
+		testPasswordSaving("derp", "derpderp", "testFile.txt", true);
+		
+		System.out.printf("Retrieval Test #1: %s%n", (testPasswordRetrieval("testAccount", "abcdefghijkl", "testFile.txt") ? "Password retrieved and matched (success)" : "Password not retrieved or not matched (failure)"));
+		System.out.printf("Retrieval Test #2: %s%n", (testPasswordRetrieval("testAccount", "helloworld", "testFile.txt") ? "Password retrieved and matched (failure)" : "Password not retrieved or not matched (success)"));
+		System.out.printf("Retrieval Test #3: %s%n", (testPasswordRetrieval("shannon.weir", "helloWorld", "testFile.txt") ? "Password retrieved and matched (success)" : "Password not retrieved or not matched (failure)"));
+	}
+	
+	private static void testPasswordHashing(String password) {
+		String testHash1 = "abcdefghijk";
+		String testHash2 = "abcdefghijklmn";
+		String testHash3 = "ThisIsATest";
+		String testHash4 = "abcdefghijkl";
+		String testHash5 = "lkjihgfedcba";
+		
+		int hash1 = password.hashCode();
+		int hash2 = testHash4.hashCode();
+		System.out.printf("Hash 1 = %d%nHash 2 = %d%n%n", hash1, hash2);
+		
+		byte[] passHash = Security.hash(password);
+		
+		boolean match1 = Security.verifyPassword(testHash1, passHash);
+		boolean match2 = Security.verifyPassword(testHash2, passHash);
+		boolean match3 = Security.verifyPassword(testHash3, passHash);
+		boolean match4 = Security.verifyPassword(testHash4, passHash);
+		boolean match5 = Security.verifyPassword(testHash5, passHash);
+		
+		System.out.printf("Test #1: %s%n", (match1 ? "Password matches (failure)" : "Password does not match (success)"));
+		System.out.printf("Test #2: %s%n", (match2 ? "Password matches (failure)" : "Password does not match (success)"));
+		System.out.printf("Test #3: %s%n", (match3 ? "Password matches (failure)" : "Password does not match (success)"));
+		System.out.printf("Test #4: %s%n", (match4 ? "Password matches (success)" : "Password does not match (failure)"));
+		System.out.printf("Test #5: %s%n", (match5 ? "Password matches (failure)" : "Password does not match (success)"));
+	}
+	
+	private static void testPasswordSaving(String account, String password, String fileName, boolean append) {	
+		try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName, append))){
+			String hashPass = Base64.getEncoder().encodeToString(Security.hash(password));
+			
+			writer.write(account);
+			writer.newLine();
+			writer.write(hashPass);
+			writer.newLine();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private static boolean testPasswordRetrieval(String account, String password, String fileName) {
+		try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+			String retAccount = null;
+			String retPass = null;
+			
+			String line1 = "";
+			String line2 = "";
+			while((line1 = br.readLine()) != null &&
+					(line2 = br.readLine()) != null) {
+				if(line1.equals(account)) {
+					retAccount = line1;
+					retPass = line2;
+					break;
+				}
+			}
+			
+			if(retAccount != null && retPass != null) {
+				//The account name was found.
+				if(retAccount.equals(account)) {
+					return Security.verifyPassword(password, Base64.getDecoder().decode(retPass));
+				}
+			}
+			else
+			{
+				//Account was not found in file.
+				return false;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		//Error occurred during file reading.
+		return false;
+	}
+}

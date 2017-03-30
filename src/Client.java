14fdb77bd378ebfb1f76606fb4d2454b0f53dfc1
Elliot Allen <eagamezz@gmail.com>
2017-03-30 16:39:21 -0400

    SSL Socket
    Replaced sockets with SSL sockets. The keystore/truststore is the same which I'm not sure is right.

--- src/Client.java
+++ src/Client.java
@@ -2,6 +2,10 @@ import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 
+import javax.net.SocketFactory;
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.SSLSocketFactory;
+
 /**
  * The main executable for the client. 
  * Establishes a secure connection socket to server.
@@ -15,6 +19,9 @@ public class Client {
 	public static void main(String args[]) throws IOException {
 		//TODO: Create View, Model & Proxy
 		
+		System.setProperty("javax.net.ssl.trustStore", "casinostore");
+		System.setProperty("javax.net.ssl.trustStorePassword", "casinostore");
+		
 		String hostname = "localhost";
 		int hostPort = 5009;
 		
@@ -22,9 +29,8 @@ public class Client {
 		//CasinoView view = new CasinoView(model);
 		Log view = new Log(model);
 		
-		Socket socket = new Socket();
-		socket.connect(new InetSocketAddress("localhost", hostPort));
-		//socket.setTcpNoDelay(true);
+		SSLSocket socket = (SSLSocket) SSLSocketFactory.getDefault().createSocket("localhost", hostPort);
+
 		CasinoModelProxy proxy = new CasinoModelProxy(socket);
 		proxy.setModelListener(model);
 		view.setViewListener(proxy);
--- src/Server.java
+++ src/Server.java
@@ -3,6 +3,8 @@ import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+import javax.net.ssl.SSLServerSocketFactory;
+
 /**
  * The main executable for the server. Starts accepting authentication requests.
  * @author Elliot Allen
@@ -11,19 +13,21 @@ import java.net.Socket;
 public class Server {
 	
 	public static void main(String[] args) throws IOException {
+		System.setProperty("javax.net.ssl.keyStore", "casinostore");
+		System.setProperty("javax.net.ssl.keyStorePassword", "casinostore");
+		
 		String host = "localhost"; //TODO: Input 
 		int port = 5009; //TODO: Port
 		
-		ServerSocket serversocket = new ServerSocket();
+		//ServerSocket serversocket = new ServerSocket();
+		ServerSocket serversocket = SSLServerSocketFactory.getDefault().createServerSocket();
 		serversocket.bind(new InetSocketAddress(host, port));
 
-		
 		
 		CasinoSessionManager manager = new CasinoSessionManager();
 
 		for (;;) {	
 				Socket socket = serversocket.accept();
-				//socket.setTcpNoDelay(true);
 				CasinoViewProxy proxy = new CasinoViewProxy (socket);
 				proxy.setViewListener ((ViewListener)manager);
 		}
